44334c147903d9e5c31ed483bd89aa8624fd9faf
steffkes <stefan@gelungen.es>
2023-07-04 11:23:46 +0200

    upgrade to nuxt3

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,36 +1,18 @@
-export default {
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
+export default defineNuxtConfig({
+  vite: {
+    server: {
+      hmr: {
+        clientPort: 3701,
+      },
+    },
   },
-
-  head: {
-    title: null,
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
-
-  components: true,
-
-  modules: ["nuxt-buefy", "@nuxtjs/axios", "@nuxtjs/proxy"],
-
-  axios: {
-    baseURL: "/",
-  },
-
+  css: ["@/node_modules/bulma/css/bulma.min.css"],
+  modules: ["nuxt-proxy"],
   proxy: {
-    "/media": {
+    options: {
       target: "https://pbs.twimg.com",
-      changeOrigin: true,
-    },
-    "/profile_images": {
-      target: "https://pbs.twimg.com",
+      pathFilter: ["/media", "/profile_images"],
       changeOrigin: true,
     },
   },
-};
+});
--- pages/embed/[screen_name]/status/[tweet_id].vue
+++ pages/embed/[screen_name]/status/[tweet_id].vue
@@ -34,24 +34,16 @@
   </div>
 </template>
 
-<script>
-export default {
-  data: () => ({}),
-  methods: {
-    clean: (url) => {
-      const local = new URL(url);
-      return local.pathname + local.search;
-    },
-  },
-  async asyncData({ params, $axios }) {
-    const tweet = await $axios.$get(
-      `https://cdn.syndication.twimg.com/tweet-result?features=tfw_timeline_list%3A%3Btfw_follower_count_sunset%3Atrue%3Btfw_tweet_edit_backend%3Aon%3Btfw_refsrc_session%3Aon%3Btfw_show_business_verified_badge%3Aon%3Btfw_mixed_media_15897%3Atreatment%3Btfw_experiments_cookie_expiration%3A1209600%3Btfw_duplicate_scribes_to_settings%3Aon%3Btfw_video_hls_dynamic_manifests_15082%3Atrue_bitrate%3Btfw_show_blue_verified_badge%3Aon%3Btfw_legacy_timeline_sunset%3Atrue%3Btfw_show_gov_verified_badge%3Aon%3Btfw_show_business_affiliate_badge%3Aon%3Btfw_tweet_edit_frontend%3Aon&lang=en&id=` +
-        params.tweet_id
-    );
+<script setup>
+const route = useRoute();
 
-    return {
-      tweet,
-    };
-  },
+const { data: tweet } = await useFetch(
+  `https://cdn.syndication.twimg.com/tweet-result?features=tfw_timeline_list%3A%3Btfw_follower_count_sunset%3Atrue%3Btfw_tweet_edit_backend%3Aon%3Btfw_refsrc_session%3Aon%3Btfw_show_business_verified_badge%3Aon%3Btfw_mixed_media_15897%3Atreatment%3Btfw_experiments_cookie_expiration%3A1209600%3Btfw_duplicate_scribes_to_settings%3Aon%3Btfw_video_hls_dynamic_manifests_15082%3Atrue_bitrate%3Btfw_show_blue_verified_badge%3Aon%3Btfw_legacy_timeline_sunset%3Atrue%3Btfw_show_gov_verified_badge%3Aon%3Btfw_show_business_affiliate_badge%3Aon%3Btfw_tweet_edit_frontend%3Aon&lang=en&id=` +
+    route.params.tweet_id
+);
+
+const clean = (url) => {
+  const local = new URL(url);
+  return local.pathname + local.search;
 };
 </script>
